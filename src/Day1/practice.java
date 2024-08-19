cd3b39306bd01a51ef3fae02f8a37293dac91c21
Abdullah MOHAMMADI <ab.mohammadi791@gmail.com>
2024-08-19 17:18:42 -0400

    My initial push to my GitHub

---
+++ src/Day1/practice.java
@@ -0,0 +1,15 @@
+package Day1;
+
+public class practice {
+    public static void main(String[] args) {
+        int num = 43;
+        int num1 = 78;
+        int sum = num + num1;
+        System.out.println(sum);
+
+    }
+
+
+
+
+}
